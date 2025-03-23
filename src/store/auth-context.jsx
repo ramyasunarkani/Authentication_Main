b7c6013dc682107c617a65000b3ff4ea93a98c92
ramyasunarkani <ramyakrishnasunarkani@gmail.com>
2025-03-23 10:22:43 +0530

    navigation guardian

--- src/store/auth-context.jsx
+++ src/store/auth-context.jsx
@@ -1,20 +1,30 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 const AuthContext=createContext({
     token:'',
-    isLogedIn:false,
+    isLoggedIn:false,
     login:(token)=>{},
     logout:()=>{}
 });
  export const AuthContextProvider=(props)=>{
-    const [token ,setToken]=useState(null);
+    const initialToken=localStorage.getItem('token');
+    const [token ,setToken]=useState(initialToken);
     const userIsLoggedIn=!!token;
     const loginHandler=(token)=>{
         setToken(token);
+        localStorage.setItem('token',token);
     }
     const logoutHandler=()=>{
         setToken(null);
+        localStorage.removeItem('token')
     }
+    useEffect(()=>{
+        const storedToken=localStorage.getItem('token');
+        if(storedToken){
+            setToken(storedToken)
+        }
+
+    },[])
     const contextValue={
         token:token,
         isLoggedIn:userIsLoggedIn,
